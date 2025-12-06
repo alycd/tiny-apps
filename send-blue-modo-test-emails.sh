32545ef0f75bce940d5167f800b08cc2de22e400
Aly <aly.sidi@tinyemail.com>
2025-12-06 13:36:53 -0800

    feat: add send-blue-modo-test-emails.sh for batch test emails, backup original send-email.sh, and clean up swaks command in send-email.sh

---
+++ send-blue-modo-test-emails.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# source /Users/alysidi/.zshrc
+
+SMTP_USER="ailani.bm@em.nestsupportgroup.com" SMTP_PASSWORD=$SMTP_PASSWORD FROM_EMAIL="ailani.bm@em.nestsupportgroup.com" TO_EMAILS="alysidi@gmail.com,deliverability@tinyemail.com" send-email.sh
+
+SMTP_USER="bryce.bgm@em.savvyfundssaver.com" SMTP_PASSWORD=$SMTP_PASSWORD FROM_EMAIL="abryce.bgm@em.savvyfundssaver.com" TO_EMAILS="alysidi@gmail.com,deliverability@tinyemail.com" send-email.sh
+
